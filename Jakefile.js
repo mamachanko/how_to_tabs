2d1f9a1138e45fa6308d160ea9381e4f58b74c0d
mamachanko <sugardubz@gmail.com>
2016-04-14 19:11:28 +0000

    introduce minimal Jake setup

---
+++ Jakefile.js
@@ -0,0 +1,13 @@
+/**
+ * Created by max on 14/04/16.
+ */
+(function () {
+    "use strict";
+
+
+    desc("Default build")
+    task("default", function() {
+        console.log("\n\nBUILD OK");
+    });
+
+}());
--- build.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by max on 14/04/16.
- */
-(function () {
-    "use strict";
-
-    console.log("\n\nBUILD OK");
-
-}());
