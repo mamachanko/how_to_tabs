bb6f67f956f04fe0d9287ab8582f55262052055e
mamachanko <sugardubz@gmail.com>
2016-04-15 19:27:01 +0000

    read expected Node version from package.json in build

--- Jakefile.js
+++ Jakefile.js
@@ -4,7 +4,6 @@
 (function () {
     "use strict";
 
-    var EXPECTED_NODE_VERSION = "v0.12.13";
 
     desc("Default build")
     task("default", ["version"], function() {
@@ -15,10 +14,12 @@
     task("version", function () {
         console.log("Checking Node version: .");
 
+        var packageJson = require("./package.json");
+        var expectedVersion = "v" + packageJson.engines.node;
         var actualVersion = process.version;
 
-        if (actualVersion !== EXPECTED_NODE_VERSION) {
-            fail("Incorrect Node version: expected " + EXPECTED_NODE_VERSION + " but was " + actualVersion);
+        if (actualVersion !== expectedVersion) {
+            fail("Incorrect Node version: expected " + expectedVersion + " but was " + actualVersion);
         }
     })
 
